d85b5a1bfb65bf195dde02956025c4e6e93fdf33
unknown <demonjack9@mail.ru>
2022-02-03 00:07:27 +0200

    Added request fields for fetch scheduler

--- diploma1/diploma1/src/DisplayTable.js
+++ diploma1/diploma1/src/DisplayTable.js
@@ -1,19 +1,57 @@
 import React from "react";
 
+const days = {
+  "0": "Monday",
+  "1": "Tuesday",
+  "2": "Wensday",
+  "3": "Thurstday",
+  "4": "Friday",
+  "5": "Saturday",
+};
+
+const studentGroups = {
+  "0": "Masterat-1",
+  "1": "Masterat-2",
+  "2": "TI-11",
+  "3": "TI-21",
+  "4": "TI-31",
+  "5": "TI-41",
+};
+
 class DisplayTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       list: [],
+      selectedDay: "",
+      selectedGroup: "",
     };
 
     this.callAPI = this.callAPI.bind(this);
     this.callAPI();
   }
 
+  handleChangeOnDay = (e) => {
+    const dayId = e.target.value;
+    const dayText = days[dayId];
+    console.log(dayText); // Do what you need to with the value here
+    this.setState({ selectedDay: dayText });
+  };
+
+  handleChangeOnGroup = (e) => {
+    const groupId = e.target.value;
+    const groupText = studentGroups[groupId];
+    console.log(groupText); // Do what you need to with the value here
+    this.setState({ selectedGroup: groupText });
+  };
+
+  writeRequest() {
+    console.log(this.state.selectedDay);
+    console.log(this.state.selectedGroup);
+  }
+
   callAPI() {
     //fetch data from API
-    //TODO make this works
     fetch(
       "https://cors-anywhere.herokuapp.com/http://dummy.restapiexample.com/api/v1/employees",
       {
@@ -45,9 +83,6 @@ class DisplayTable extends React.Component {
           <td>{item.employee_name}</td>
           <td>{item.emplotee_salary}</td>
           <td>{item.employee_age}</td>
-          <td>
-            <button className="btn">Remove</button>
-          </td>
         </tr>
       );
     });
@@ -60,11 +95,39 @@ class DisplayTable extends React.Component {
               <th>Name</th>
               <th>Salary</th>
               <th>Age</th>
-              <th>Remove</th>
             </tr>
           </thead>
           <tbody>{tb_data}</tbody>
         </table>
+        <div className="outer">
+          <select
+            value={this.state.selectedDay}
+            onChange={this.handleChangeOnDay}
+            style={{ width: "25%" }}
+          >
+            <option value="0">Monday</option>
+            <option value="1">Tuesday</option>
+            <option value="2">Wensday</option>
+            <option value="3">Thurstday</option>
+            <option value="4">Friday</option>
+            <option value="5">Saturday</option>
+          </select>
+          <select
+            value={this.state.selectedGroup}
+            onChange={this.handleChangeOnGroup}
+            style={{ width: "25%" }}
+          >
+            <option value="0">Masterat-1</option>
+            <option value="1">Masterat-2</option>
+            <option value="2">TI-11</option>
+            <option value="3">TI-21</option>
+            <option value="4">TI-31</option>
+            <option value="5">TI-41</option>
+          </select>
+          <button className="dbtn" onClick={() => this.writeRequest()}>
+            SEND
+          </button>
+        </div>
       </div>
     );
   }
