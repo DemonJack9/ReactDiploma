3a77d8cd1e33e3c5fb6206406e2038bc3b557ee7
unknown <demonjack9@mail.ru>
2022-02-12 01:54:31 +0200

    new two select options with faculty and group

--- diploma1/diploma1/src/DisplayTable.js
+++ diploma1/diploma1/src/DisplayTable.js
@@ -23,12 +23,36 @@ class DisplayTable extends React.Component {
     super(props);
     this.state = {
       list: [],
-      selectedDay: "",
-      selectedGroup: "",
+      faculties: [],
+      groups: [],
+      selectedDay: "Monday",
+      selectedGroup: "TAW-1",
+      selectedFaculty: "Information Technology",
     };
 
     this.callAPI = this.callAPI.bind(this);
+    this.changeFaculty = this.changeFaculty.bind(this);
     this.callAPI();
+  }
+
+  componentDidMount() {
+    this.setState({
+      faculties: [
+        {
+          name: "Information Technology",
+          groups: ["TAW-1", "TAW-2", "TI-11", "TI-21", "TI-31", "TI-41"],
+        },
+        {
+          name: "Economics",
+          groups: ["EC1", "EC2", "EC3"],
+        },
+        {
+          name: "Medicine",
+          groups: ["MED-1", "MED-2", "MED-3"],
+        },
+      ],
+      groups: ["TAW-1", "TAW-2", "TI-11", "TI-21", "TI-31", "TI-41"],
+    });
   }
 
   handleChangeOnDay = (e) => {
@@ -37,15 +61,31 @@ class DisplayTable extends React.Component {
     this.setState({ selectedDay: dayText });
   };
 
-  handleChangeOnGroup = (e) => {
-    const groupId = e.target.value;
-    const groupText = studentGroups[groupId];
-    this.setState({ selectedGroup: groupText });
+  changeFaculty(event) {
+    this.setState({ selectedFaculty: event.target.value });
+
+    this.setState({
+      groups: this.state.faculties.find(
+        (cntry) => cntry.name === event.target.value
+      ).groups,
+    });
+
+    const stats = this.state.faculties.find(
+      (cntry) => cntry.name === this.state.selectedFaculty
+    ).groups;
+    this.setState({
+      groups: stats.find((stat) => stat.name === event.target.value).groups,
+    });
+  }
+
+  changeGroup = (e) => {
+    this.setState({ selectedGroup: e.target.value });
   };
 
   writeRequest() {
     console.log(this.state.selectedDay);
     console.log(this.state.selectedGroup);
+    console.log(this.state.selectedFaculty);
   }
 
   callAPI() {
@@ -101,7 +141,7 @@ class DisplayTable extends React.Component {
           <select
             value={this.selectedDay}
             onChange={this.handleChangeOnDay}
-            style={{ width: "25%" }}
+            style={{ width: "20%" }}
           >
             <option value="0">Monday</option>
             <option value="1">Tuesday</option>
@@ -111,16 +151,18 @@ class DisplayTable extends React.Component {
             <option value="5">Saturday</option>
           </select>
           <select
-            value={this.selectedGroup}
-            onChange={this.handleChangeOnGroup}
-            style={{ width: "25%" }}
+            value={this.state.selectedFaculty}
+            onChange={this.changeFaculty}
+            style={{ width: "20%" }}
           >
-            <option value="0">Masterat-1</option>
-            <option value="1">Masterat-2</option>
-            <option value="2">TI-11</option>
-            <option value="3">TI-21</option>
-            <option value="4">TI-31</option>
-            <option value="5">TI-41</option>
+            {this.state.faculties.map((e, key) => {
+              return <option key={key}>{e.name}</option>;
+            })}
+          </select>
+          <select style={{ width: "20%" }} onChange={this.changeGroup}>
+            {this.state.groups.map((e, key) => {
+              return <option key={key}>{e}</option>;
+            })}
           </select>
           <button
             style={{ width: "15%" }}
